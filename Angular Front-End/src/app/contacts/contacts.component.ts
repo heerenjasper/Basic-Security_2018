51f858e6c549edcfd5edc53b6b5787c42ae414d8
EbertJoris <ebert-joris@hotmail.com>
2018-04-19 10:18:12 +0200

    Edited front-end for more mobile friendly user

--- Angular Front-End/src/app/contacts/contacts.component.ts
+++ Angular Front-End/src/app/contacts/contacts.component.ts
@@ -1,6 +1,7 @@
-import { Component} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { AuthService } from '../core/auth.service';
 
 // custom user interface
 interface User {
@@ -15,19 +16,32 @@ interface User {
   styleUrls: ['./contacts.component.scss']
 })
 
-export class ContactsComponent {
+export class ContactsComponent implements OnInit {
   private usersCollection: AngularFirestoreCollection<User>;
   users: Observable<User[]>;
+  currentUser: User;
+  selectedUser: User;
 
-  constructor(private afs: AngularFirestore) { 
+  constructor(private afs: AngularFirestore,
+  private authService: AuthService) { 
 
     this.usersCollection = this.afs.collection<User>('users');
     this.users = this.usersCollection.valueChanges();
 
   }
 
+  
+  ngOnInit() {    
+    this.authService.user.subscribe(user => this.currentUser = user);
+  }
+
   addItem(user: User) {
     this.usersCollection.add(user);
+  }
+
+  selectChat(user: User) {
+    console.log(user.photoURL);
+    this.selectedUser = user;
   }
 
 }
